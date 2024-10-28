789c05e6a6711945b18c4364e4a7d3ca8c969cf9
laurabpaulsen <202005791@post.au.dk>
2024-10-28 10:07:16 +0100

    setting up env

---
+++ setup_env.sh
@@ -0,0 +1,5 @@
+scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+echo $scriptDir
+python3 -m venv "$scriptDir/env"
+source "$scriptDir/env/bin/activate"
+python3 -m pip install -r "$scriptDir/requirements.txt"
