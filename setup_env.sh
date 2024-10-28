50c82d95cdbee5a861140d1f7c45804d331fc2fd
laurabpaulsen <202005791@post.au.dk>
2024-10-28 13:56:23 +0100

    add upgrade of pip

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,9 @@
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 echo $scriptDir
+
 python3 -m venv "$scriptDir/env"
+
 source "$scriptDir/env/bin/activate"
+python3 -m pip install --upgrade pip
+pip3 install psychopy --no-deps
 python3 -m pip install -r "$scriptDir/requirements.txt"
