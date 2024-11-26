4ce0a53888422f98e9e9794a5b63cde7ac8b70bd
laurabpaulsen <202005791@post.au.dk>
2024-11-26 11:02:23 +0100

    add python installation

--- setup_env_windows.cmd
+++ setup_env_windows.cmd
@@ -1,8 +1,8 @@
 @echo off
 set scriptDir=%~dp0
 set scriptDir=%scriptDir:~0,-1%
-
-python -m venv "%scriptDir%\env"
+winget install -e --id Python.Python.3.11
+python3 -m venv "%scriptDir%\env"
 
 call "%scriptDir%\env\Scripts\activate.bat"
 python -m pip install --upgrade pip
