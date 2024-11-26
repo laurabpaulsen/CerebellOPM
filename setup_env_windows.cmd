4da9e38548cd36795ac0be8b440af956ace45a59
laurabpaulsen <202005791@post.au.dk>
2024-11-26 11:18:16 +0100

    update

--- setup_env_windows.cmd
+++ setup_env_windows.cmd
@@ -2,9 +2,9 @@
 set scriptDir=%~dp0
 set scriptDir=%scriptDir:~0,-1%
 winget install -e --id Python.Python.3.11
-python3 -m venv "%scriptDir%\env"
+py -m venv "%scriptDir%\env"
 
 call "%scriptDir%\env\Scripts\activate.bat"
-python3 -m pip install --upgrade pip
+py -m pip install --upgrade pip
 pip3 install psychopy --no-deps
 pip3 install -r "%scriptDir%\requirements.txt"
