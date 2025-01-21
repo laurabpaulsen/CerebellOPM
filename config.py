75bda060dd13c2df8ab09f20866a9c4827cd4912
laurabpaulsen <202005791@post.au.dk>
2025-01-21 13:22:41 +0100

    add config with channel specs

---
+++ config.py
@@ -0,0 +1,13 @@
+fiducials = ["lpa", "rpa", "nasion"]
+OPM_sensors = ['FL57', 'FL58', 'FL59', 'FL60', 'FL61', 'FL83', 'FL84', 'FL98', 'FL99', 'FL102', 'FL103', 'FL104', 'FL105', 'FL106', 'FL107']
+EEG_sensors = [ "Cz", "C3", "F3", "FC1", "CP1", "CP5"]
+
+cap_ch_names = [
+        "Fp1", "Fp2", 
+        "F7", "F3", "Fz", "F4", "F8",
+        "FT9", "FC5", "FC1", "FC2", "FC6", "FT10",
+        "T7", "C3", "Cz", "C4", "T8",
+        "TP9", "CP5", "CP1", "CP2", "CP6", "TP10",
+        "P7", "P3", "Pz", "P4", "P8",
+        "O1", "Oz", "O2"
+        ]
